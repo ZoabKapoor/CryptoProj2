cadbd97f308c8c80bf0c04527e3ccb6284674a49
ZoabKapoor <zoab95@hotmail.com>
2016-04-21 11:15:14 +0200

    cobbled together and inelegant, but it works. Also uses DES

--- src/cryptography/Cryptographer.java
+++ src/cryptography/Cryptographer.java
@@ -1,34 +1,61 @@
 package cryptography;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import java.io.IOException;
-import java.nio.file.Files;
 
 public class Cryptographer {
 	
-	private final String ALGORITHM = "AES";
-	private final String CRYPTOMODE = "AES/CBC/PKCS5Padding";
-	private Key KEY;
+	private final String ALGORITHM = "DES";
+	private final String CRYPTOMODE = "DES/CBC/PKCS5Padding";
+	// TODO - Change crypto mode to more secure mode, figure out how to construct key for AES
+//	private final int KEYLENGTH = 256;
+//	private final int ITERATIONS = 65536;
+	private SecretKey key;
+//	private final byte[] SALT = "0".getBytes();
+	
+	// static SecureRandom rnd = new SecureRandom();
+
+	// 8 is a magic number - fix
+	static IvParameterSpec iv = new IvParameterSpec(new byte[8]);
 	
 	byte[] bytes;
 	
-	public Cryptographer(String key, Path p) {
-			KEY = new SecretKeySpec(key.getBytes(), ALGORITHM);
+	public Cryptographer(String keyString, Path p) {
+		key = generateKey(keyString);
 		try {
 			bytes = Files.readAllBytes(p);
 		} catch (IOException e) {
-			throw new RuntimeException("Couldn't read the file with path " + p + " . Is the file path correct?");
+			throw new RuntimeException("Couldn't read the file with path " + p + " . Is the file path correct?", e);
+		}
+	}
+	
+	public SecretKey generateKey(String keyString) {
+		try {
+			SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+			KeySpec spec = new DESKeySpec(keyString.getBytes());
+			SecretKey tmp = factory.generateSecret(spec);
+			SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "DES");
+			return secret;
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException e) {
+			throw new RuntimeException("Key could not be generated!", e);
 		}
 	}
 	
@@ -36,29 +63,30 @@ public class Cryptographer {
 		try {
 			bytes = Files.readAllBytes(p);
 		} catch (IOException e) {
-			throw new RuntimeException("Couldn't read the file with path " + p + " . Is the file path correct?");
+			throw new RuntimeException("Couldn't read the file with path " + p + " . Is the file path correct?", e);
 		}
 	}
 	
-	public void changeKey(String newKey) {
-		KEY = new SecretKeySpec(newKey.getBytes(), ALGORITHM);
+	public void changeKey(String newKeyString) {
+		key = generateKey(newKeyString);
 	}
 	
 	public void doCrypto(Path out, int mode) {
 			Cipher cipher = createCipher();
 			try {
 				if (mode == Cipher.ENCRYPT_MODE) {
-					cipher.init(Cipher.ENCRYPT_MODE, KEY);
+					cipher.init(Cipher.ENCRYPT_MODE, key, iv);
 				} else if (mode == Cipher.DECRYPT_MODE) {
-					cipher.init(Cipher.DECRYPT_MODE, KEY);
+					cipher.init(Cipher.DECRYPT_MODE, key, iv);
 				} else {
 					throw new IllegalArgumentException("The mode specified is not valid");
 				}
-			} catch (InvalidKeyException e) {
-				throw new RuntimeException("The key " + KEY + "is not valid! Check that it's the correct length");
+			} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+				throw new RuntimeException("The key " + key + " is not valid!", e);
 			}
 			try {
 				byte[] output = cipher.doFinal(bytes);
+				Files.createFile(out);
 				Files.write(out, output, StandardOpenOption.WRITE);
 			} catch (IllegalBlockSizeException | BadPaddingException | IOException e) {
 				throw new RuntimeException("Crypto failed!", e);
@@ -73,5 +101,16 @@ public class Cryptographer {
 		} catch (NoSuchPaddingException e) {
 			throw new RuntimeException("The padding schema requested is unavailable!", e);
 		}
+	}
+	
+	public static void main(String[] args) {
+		String keystr = "password";
+		Path inputPath;
+		Path outputPath;
+		inputPath = Paths.get(".", "output.txt");
+		outputPath = Paths.get(".","decrypted.txt");
+		Cryptographer encoder = new Cryptographer(keystr, inputPath);
+		encoder.doCrypto(outputPath, Cipher.DECRYPT_MODE);
+		System.out.println("Done!");
 	}
 }
