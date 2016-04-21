753e16447d18daf7b75c1527be8f31ca3ceec14e
juliamcarr <juliamcarr1@gmail.com>
2016-04-21 23:47:30 +0200

    edits to uploader file

--- src/transfer/UploaderDownloader.java
+++ src/transfer/UploaderDownloader.java
@@ -1,5 +1,29 @@
 package transfer;
 
+
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
+
 public class UploaderDownloader {
+
+    //blob credentials
+
+    public static final String storageConnectionString =
+            "DefaultEndpointsProtocol=http;" +
+                    "AccountName=your_storage_account;" +
+                    "AccountKey=your_storage_account_key";
+
+
+    //blob init
+
+
+    //blob container init
+
+
+    //if no container exists
+
+
+    //blob upload
+
 
 }
