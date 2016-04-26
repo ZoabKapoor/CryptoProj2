7075a65b9d49897ecc468f4e683471203535758d
juliamcarr <juliamcarr1@gmail.com>
2016-04-26 21:42:27 +0200

    Upload of Blob now working

--- src/transfer/UploaderDownloader.java
+++ src/transfer/UploaderDownloader.java
@@ -4,26 +4,49 @@ package transfer;
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
 
+
 public class UploaderDownloader {
+	
+	//blob credentials
+	public static final String storageConnectionString =
+			"DefaultEndpointsProtocol=http;" +
+					"AccountName=juliamcarr;" +
+					"AccountKey=SSpOZPJ5PJx+f/ehu58tf8jam+HRZo3Dpq1/+SvFT8mHBOWbXIN25e4lHadRR2Teq0i/JD4909PJNy30BEAfWA==";
+	
+	//Start connection with associated azure account and create a container
+	public static void uploadBlob(String containerName, String fileReference, String blobName) {
+			try
+			{
+				// Retrieve storage account from connection-string.
+				CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);  
+			   
+				// Create the blob client.
+				   CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+				   
+				// Get a reference to a container.
+				// The container name must be lower case
+			   CloudBlobContainer container = blobClient.getContainerReference(containerName.toLowerCase());
 
-    //blob credentials
+			   // Create the container if it does not exist.
+			    container.createIfNotExists();
 
-    public static final String storageConnectionString =
-            "DefaultEndpointsProtocol=http;" +
-                    "AccountName=your_storage_account;" +
-                    "AccountKey=your_storage_account_key";
+			    // Create or overwrite the "myimage.jpg" blob with contents from a local file.
+			    CloudBlockBlob blob = container.getBlockBlobReference(blobName);
+			   
+			    blob.uploadFromFile(fileReference);
 
+			}
+			catch (Exception e)
+			{
+			    // Output the stack trace.
+			    e.printStackTrace();
+			}
+		
+	}
+	public static void main(String[] args) {
+		uploadBlob("JuliasBlobs", "/Users/juliamcarr/Documents/workspace/CryptoProj2/output.txt", "output");
+		System.out.println("All done! You have uploaded a blob!");
 
-    //blob init
-
-
-    //blob container init
-
-
-    //if no container exists
-
-
-    //blob upload
-
+	}
 
 }
