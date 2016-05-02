8d9c46bba6e21079daf59ea960c9cb2403ea75a3
juliamcarr <juliamcarr1@gmail.com>
2016-05-02 10:08:20 +0200

    Added download

--- src/transfer/UploaderDownloader.java
+++ src/transfer/UploaderDownloader.java
@@ -1,6 +1,8 @@
 package transfer;
 
 
+import java.io.FileOutputStream;
+
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
 
@@ -12,10 +14,15 @@ public class UploaderDownloader {
 	static String blobName;
 	
 	//blob credentials
-	public static final String storageConnectionString =
-			"DefaultEndpointsProtocol=http;" +
-					"AccountName=juliamcarr;" +
-					"AccountKey=SSpOZPJ5PJx+f/ehu58tf8jam+HRZo3Dpq1/+SvFT8mHBOWbXIN25e4lHadRR2Teq0i/JD4909PJNy30BEAfWA==";
+	public static String storageConnectionString =
+		"DefaultEndpointsProtocol=http;" +
+				"AccountName=;" +
+				"AccountKey=";
+	
+	public static void storageConnection(String userName, String key) {
+		storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=" 
+	+ userName + ";" + "AccountKey=" + key;
+	}
 	
 	//Start connection with associated azure account and create a container
 	public static void uploadBlob() {
@@ -38,6 +45,18 @@ public class UploaderDownloader {
 			    CloudBlockBlob blob = container.getBlockBlobReference(blobName);
 			   
 			    blob.uploadFromFile(fileReference);
+			    
+			    for (ListBlobItem blobItem : container.listBlobs()) {
+				       System.out.println(blobItem.getUri());
+				   }
+			    for (ListBlobItem blobItem : container.listBlobs()) {
+			        // If the item is a blob, not a virtual directory.
+			        if (blobItem instanceof CloudBlob) {
+			            // Download the item and save it to a file with the same name.
+			             CloudBlob blob1 = (CloudBlob) blobItem;
+			             blob.download(new FileOutputStream("/Users/juliamcarr/Documents/Test/" + blob1.getName()));
+			         }
+			     }
 
 			}
 			catch (Exception e)
@@ -47,14 +66,47 @@ public class UploaderDownloader {
 			}
 		
 	}
+	
+	public static void downloadBlobs() {
+		try
+		{
+		    // Retrieve storage account from connection-string.
+		   CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+
+		   // Create the blob client.
+		   CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+		   // Retrieve reference to a previously created container.
+		   CloudBlobContainer container = blobClient.getContainerReference(containerName);
+
+		   for (ListBlobItem blobItem : container.listBlobs()) {
+			    // If the item is a blob, not a virtual directory
+			    if (blobItem instanceof CloudBlockBlob) {
+			        // Download the text
+			    	CloudBlockBlob retrievedBlob = (CloudBlockBlob) blobItem;
+			    	System.out.println(retrievedBlob.getName());
+			    }
+			}
+		}
+		catch (Exception e)
+		{
+		    // Output the stack trace.
+		    e.printStackTrace();
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
+		storageConnection("juliamcarr",
+				"SSpOZPJ5PJx+f/ehu58tf8jam+HRZo3Dpq1/+SvFT8mHBOWbXIN25e4lHadRR2Teq0i/JD4909PJNy30BEAfWA==");
 		containerName = "JuliasBlob2";
-		blobName = "output";
-		fileReference = "/Users/juliamcarr/Documents/workspace/CryptoProj2/output.txt";
+		blobName = "input";
+		fileReference = "/Users/juliamcarr/Documents/workspace/CryptoProj2/input.txt";
 		
 		uploadBlob();
 		System.out.println("All done! You have uploaded a blob named " + blobName);
-
+		
 	}
 
 }
